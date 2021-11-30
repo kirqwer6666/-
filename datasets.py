308eb76b2faf2e9c2e4de5c57f2dd51526ff139a
kirqwer6666 <71703688+kirqwer6666@users.noreply.github.com>
2021-11-30 15:57:35 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,15 +1,28 @@
-from PIL import Image
-import torch
-import numpy as np
-from matplotlib import pyplot as plt
-from torchvision.utils import make_grid
-import torchvision
+
 import os
 import random
+from tqdm import *
 import torch
+import numpy as np
 import torch.utils.data as data
 import torchvision.transforms as transforms
 from PIL import Image
+from matplotlib import pyplot as plt
+from torchvision.utils import make_grid
+
+def tensorshow(tensors, titles=None):
+    '''
+    BCWH
+    '''
+    fig = plt.figure()
+    for tensor, tit, i in zip(tensors, titles, range(len(tensors))):
+        img = make_grid(tensor)
+        npimg = img.numpy()
+        ax = fig.add_subplot(211+i)
+        ax.imshow(np.transpose(npimg, (1, 2, 0)))
+        ax.set_title(tit)
+    plt.show()
+
 
 def get_file_paths(folder):
     file_paths = []
@@ -28,7 +41,7 @@ def augment(img_input, img_target):
     return img_input, img_target
 
 class RESIDE_Dataset(data.Dataset):
-    def __init__(self, path, train, transform, format='.jpg'):
+    def __init__(self, path, train, transform, format='.png'):
         super(RESIDE_Dataset, self).__init__()
         self.format = format
         self.transform = transform
@@ -37,66 +50,68 @@ class RESIDE_Dataset(data.Dataset):
         haze_img_dir = os.path.join(path, 'hazy')
         self.clea_img_dir = os.path.join(path, 'gt')
 
-        self.haze_file_path = get_file_paths(haze_img_dir)
+        self.haze_file_path = sorted(get_file_paths(haze_img_dir))
 
         self.n_samples = len(self.haze_file_path)
 
-    def get_img_pair(self, idx):
+    def __getitem__(self, idx):
+        # img_input, img_target = self.get_img_pair(idx)
         img_haze = Image.open(self.haze_file_path[idx]).convert('RGB')
-        img_clea = self.haze_file_path[idx].split('_')[1].split('/')[-1] + self.format
-        img_clea = Image.open(os.path.join(self.clea_img_dir, img_clea)).convert('RGB')
+        # print("haze:", self.haze_file_path[idx])
+        img_clea_num = self.haze_file_path[idx].split('_')[0].split('\\')[-1]
+        # print("img_clea_num:", img_clea_num)
+        img_clea = Image.open(os.path.join(self.clea_img_dir, img_clea_num) + self.format).convert('RGB')
+        # print("gt:", os.path.join(self.clea_img_dir, img_clea_num) + self.format)
+        if self.train:
+            img_haze, img_clea = augment(img_haze, img_clea)
+        img_haze, img_clea = self.transform(img_haze), self.transform(img_clea)
 
         return img_haze, img_clea
-
-    def __getitem__(self, idx):
-        img_input, img_target = self.get_img_pair(idx)
-        if self.train:
-            img_input, img_target = augment(img_input, img_target)
-        img_input, img_target = self.transform(img_input), self.transform(img_target)
-
-        return img_input, img_target
 
     def __len__(self):
         return self.n_samples
 
-transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+transform = transforms.Compose([transforms.Resize((512, 512)), transforms.ToTensor()])
 
 
-path = './datasets/'
-batch_size = 1
+path = 'D:/zwx/zwx/dehaze/dataset/'
+batch_size = 4
 num_workers = 8
 
-# ITS_train_loader = torch.utils.data.DataLoader(
-#     dataset=RESIDE_Dataset(path+'RESIDE/ITS', train=True, transform=transform),
-#     batch_size=batch_size,
-#     shuffle=True,
-#     num_workers=num_workers)
-# ITS_val_loader = torch.utils.data.DataLoader(
-#     dataset=RESIDE_Dataset(path+'RESIDE/SOTS/indoor', train=False, transform=transform),
-#     batch_size=batch_size,
-#     shuffle=False,
-#     num_workers=num_workers)
-# OTS_train_loader = torch.utils.data.DataLoader(
-#     dataset=RESIDE_Dataset(path+'RESIDE/OTS', train=True, transform=transform),
-#     batch_size=batch_size,
-#     shuffle=True,
-#     num_workers=num_workers)
-# OTS_val_loader = torch.utils.data.DataLoader(
-#     dataset=RESIDE_Dataset(path+'RESIDE/SOTS/outdoor', train=False, transform=transform),
-#     batch_size=batch_size,
-#     shuffle=False,
-#     num_workers=num_workers)
-#
-# IHAZY_train_loader = torch.utils.data.DataLoader(
-#     dataset=RESIDE_Dataset(path+'NTIRE/IHAZY', train=True, transform=transform),
-#     batch_size=batch_size,
-#     shuffle=True,
-#     num_workers=num_workers)
-# IHAZY_val_loader = torch.utils.data.DataLoader(
-#     dataset=RESIDE_Dataset(path+'NTIRE/TEST/indoor', train=False, transform=transform),
-#     batch_size=batch_size,
-#     shuffle=False,
-#     num_workers=num_workers)
+# RESIDE数据集
+ITS_train_loader = torch.utils.data.DataLoader(
+    dataset=RESIDE_Dataset(path+'RESIDE/ITS/train', train=True, transform=transform, format='.png'),
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers)
+ITS_val_loader = torch.utils.data.DataLoader(
+    dataset=RESIDE_Dataset(path+'RESIDE/ITS/val', train=False, transform=transform, format='.png'),
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=num_workers)
+OTS_train_loader = torch.utils.data.DataLoader(
+    dataset=RESIDE_Dataset(path+'RESIDE/OTS', train=True, transform=transform, format='.jpg'),
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers)
+OTS_val_loader = torch.utils.data.DataLoader(
+    dataset=RESIDE_Dataset(path+'RESIDE/SOTS/outdoor', train=False, transform=transform, format='.png'),
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=num_workers)
+
+
+# I-HAZE/O-HAZE CVPR2018
+IHAZY_train_loader = torch.utils.data.DataLoader(
+    dataset=RESIDE_Dataset(path+'NTIRE/IHAZE/train', train=True, transform=transform, format='.jpg'),
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers)
+IHAZY_val_loader = torch.utils.data.DataLoader(
+    dataset=RESIDE_Dataset(path+'NTIRE/IHAZE/val', train=False, transform=transform, format='.jpg'),
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=num_workers)
 # OHAZY_train_loader = torch.utils.data.DataLoader(
 #     dataset=RESIDE_Dataset(path+'NTIRE/OHAZY', train=True, transform=transform),
 #     batch_size=batch_size,
@@ -109,44 +124,18 @@ num_workers = 8
 #     num_workers=num_workers)
 
 # DENSE-HAZE CVPR2019
+
 # NH-HAZE CVPR2020
 
-class Test_Dataset(data.Dataset):
-    def __init__(self, path, transform):
-        super(Test_Dataset, self).__init__()
-        self.format = format
-        self.transform = transform
+# if __name__ == "__main__":
+#     loop = tqdm(enumerate(ITS_val_loader), total=len(ITS_train_loader))
+#     for i, (haze, gt) in loop:
+#         # loop.set_description(f'Epoch [{epoch}/{num_epochs}]')
+#         loop.set_postfix(loss=i, shape=haze.shape)
+#         # tensorshow(haze, titles='haze')
+#         # tensorshow(gt, titles='gt')
+#         # if i == 2: break
 
-        self.file_path = get_file_paths(path)
-
-        self.n_samples = len(self.file_path)
-
-    def __getitem__(self, idx):
-        img = Image.open(self.file_path[idx]).convert('RGB')
-        img_input= self.transform(img)
-        return img_input
-
-    def __len__(self):
-        return self.n_samples
-
-TestLoader = torch.utils.data.DataLoader(
-    dataset=Test_Dataset(path='./ok', transform=transform),
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=num_workers
-)
-
-def tensorshow(tensors):
-    '''
-    BCWH
-    '''
-    fig = plt.figure()
-    for tensor, i in zip(tensors, range(len(tensors))):
-        img = make_grid(tensor)
-        npimg = img.detach().numpy()
-        ax = fig.add_subplot(211+i)
-        ax.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
 
 
 
